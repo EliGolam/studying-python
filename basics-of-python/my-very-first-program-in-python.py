3efe923d44d8bf37bd3f388ef9ad19ba2c4806f6
EliGolam <elistasibusiness@gmail.com>
2022-07-09 21:06:49 +0200

    experimented a bit

--- basics-of-python/my-very-first-program-in-python.py
+++ basics-of-python/my-very-first-program-in-python.py
@@ -1,4 +1,10 @@
+# This is a comment
+name = input('Hello, what is your name? ')
+print('Hi', name)
+# User input
 user_inp = input('Europe floor number: ')
 us_floor = int(user_inp) + 1
-
+# Print on the console
 print('US floor number', us_floor)
+
+print('Oh my you sexy')
