ba79506d79840f6629124c1c8d26d389f4bb956b
EliGolam <elistasibusiness@gmail.com>
2022-07-14 05:22:05 +0200

    03-01-EX v1.2: Added more user friendly messages

--- basics-of-python/03-01-EX-better-payrate.py
+++ basics-of-python/03-01-EX-better-payrate.py
@@ -4,19 +4,21 @@ if user_inp == 'y':
     work_hours = -1.0
     
     while work_hours < 0 :
-        user_inp = input("Enter Hours worked per day: ")
+        user_inp = input("Please enter a positive number in digits.\nEnter Hours worked per day: ")
         try:
             work_hours = float (user_inp)
+            if work_hours > 0 : print("Great !!\n\n")
         except:
-            print("Please enter a positive number in digits.")
+            print("Please enter a number")
     
     pay_per_hour = -1.0
     while pay_per_hour < 0 :
-        user_inp = input("Enter you Pay per hour: ")
+        user_inp = input("Please enter a positive number in digits.\nEnter you Pay per hour: ")
         try:
             pay_per_hour = float (user_inp)
+            if pay_per_hour > 0 : print("Great !!\n\n")
         except:
-            print("Please enter a positive number in digits.")
+            print("Please enter a number")
 
     print("You earn ", work_hours*pay_per_hour, " buckaroonies every day! Nice")
 
