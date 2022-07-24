a523540a38d29c86c9b0a093880a664969012c86
EliGolam <elistasibusiness@gmail.com>
2022-07-24 23:11:36 +0200

    06-STRING:
    exercise 01

---
+++ basics-of-python/06-strings.py
@@ -0,0 +1,20 @@
+# 06 STRINGS
+s1 = "Apple"
+printStringBackwards_1(s1)
+
+# Exercise 1: 
+## Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, 
+## printing each letter on a separate line, except backwards. 
+
+### Solution 1: with the while loop we can initiate the index at len(str) - 1 and make each step be -1
+
+def printStringBackwards_1 (s: str) : 
+    index = len(s) - 1
+    while index >= 0 :
+        print(s[index])
+        # step
+        index -= 1
+    return None
+
+
+
--- basics-of-python/README.md
+++ basics-of-python/README.md
@@ -1,17 +1,3 @@
 # Python - Basics of Python
 
-Exercises and Practice 
-
-## 2022/07/14
-
-### 03-01-EX-better-payrate
-
-Rewrite payrate computation to give employees 1.5 times the hourly rate for hours worked above 40 hours.
-
-### 04-06-EX
-
-Rewrtie payrate code with functions
-
-### Practice
-
-Largest Number
+Exercises and Practice from py4e.com by Dr. Charles Severance. 
