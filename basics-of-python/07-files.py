87057398e3b813e8e7e644b129b51d595abb1a2e
EliGolam <elistasibusiness@gmail.com>
2022-08-02 05:21:03 +0200

    07-Files - Exercise 2

--- basics-of-python/07-files.py
+++ basics-of-python/07-files.py
@@ -40,3 +40,22 @@ for line in fhand :
     line = line.rstrip()
     print(line.upper())
 fhand.close()
+
+"""
+Exercise 2: Write a program to prompt for a file name, and then read through the file and look for lines of the form:
+When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart the line to extract the floating-point number on the line. Count these lines and then compute the 
+total of the spam confidence values from these lines. When you reach the end of the file, print out the average spam confidence.
+"""
+string_search = 'X-DSPAM-Confidence: '
+dspam_list = []
+file_handle = open('mbox.txt')
+for line in file_handle :
+    if line.find(string_search) == -1 : continue
+    line = line.rstrip()
+    dspam_list.append(float(line.partition(string_search)[2]))
+print(f"Average Spam Confidence: {sum(dspam_list) / len(dspam_list)}")
+file_handle.close()
+
+"""
+Exercise 3: Sometimes when programmers get bored or want to have a bit of fun, they add a harmless Easter Egg to their program. Modify the program that prompts the user for the file name so that it prints a funny message when the user types in the exact file name “na na boo boo”. The program should behave normally for all other files which exist and don’t exist. 
+"""
